bc4fd6064ae974959d78c023c1add935c06a9722
Rushikesh Phadtare <rushikeshphadtare2003@gmail.com>
2026-09-23 17:25:27 +0530

    change port number

--- app.py
+++ app.py
@@ -200,6 +200,8 @@ with gr.Blocks(
         outputs=[chatbot, message]
     )
 
-
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
